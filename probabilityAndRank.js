ef012e995f5f320c629fe82b02eb5ad5756ac1e2
Bardha Kryeziu <bk68313@ubt-uni.net>
2024-02-10 02:26:53 +0100

    Shpjegimi ne komente
    Shpjegimi i detajuar i programit dhe procesit te tij ne gjuhen shqipe, ne forme te komenteve, per secilin funksion

--- probabilityAndRank.js
+++ probabilityAndRank.js
@@ -14,6 +14,14 @@ function combinations(arr, k) {//funksioni per me i gjeneru te gjitha kombinimet
     combine(0, []);
     return result;
 }
+/*Funksioni combinations eshte funksion i cili gjeneron te gjitha kombinimet e mundshme te cfaredo madhesie k prej nje vargu arr.
+Per shembull, nese kemi vargun [1, 2, 3, 4, 5], dhe k=3, funksioni do te gjeneroje te gjitha kombinimet e mundshme te madhesise 3 prej vargut tone.
+Per me e paraqit kete rezultat, e perdor nje menyre rekursive dhe fillon me inicializiming e nje vargu bosh te quajtur result.
+Brenda combinations, e kemi nje funksion tjt quajtur combine, i cili merr dy argumente, startIndex dhe combination.
+Nese madhesia e kombinimit eshte e barabarte me k, atehere e shtojme ate ne rezultat.
+Perndryshe, per cdo element te vargut, e shtojme ate ne kombinim dhe e therrasim funksionin combine perseri, duke e rritur startIndex me 1.
+Per kete perdorim push and pop te cilat jane dukuri te stack.
+*/
 
 function isStraight(cards){ //funksion i cili e shikon nese letrat tona e krijojne nje straight
     const ranks = cards.map(card => Math.floor(card/4));
@@ -23,11 +31,21 @@ function isStraight(cards){ //funksion i cili e shikon nese letrat tona e krijoj
     const sortedRanks = Array.from(uniqueRanks).sort((a, b) => a - b);
     return sortedRanks[4] - sortedRanks[0] === 4;
 }
+/* Funksioni isStraight eshte funksion i cili e shikon nese letrat tona e krijojne nje straight.
+Ne fillim e mapon secilen leter duke u bazuar ne rangun e saj, duke e ndare me 4 dhe duke e marre vleren floor.
+Pastaj e krijojme nje set nga ranku per te i larguar duplikatet. Nese madhesia e setit nuk eshte e barabarte me 5, atehere dmth qe ka duplicate ranks 
+edhe kjo dore nuk eshte Straight. Perndryshe, e shnderron setin ne varg, e sorton ne ascending order edhe e shikon diferencen mes rankut me te larte
+dhe me te ulet, Nese diferenca eshte 4, kemi nje straight, perndryshe jo.
+ */
 
 function isFlush(cards){ //funksioni i cili e shikon nese letrat tona e krijojne nje flush
     const suits = cards.map(card => card % 4);
     return new Set(suits).size === 1;
 }
+/* Funksioni isFlush eshte funksion i cili e shikon nese letrat tona e krijojne nje flush.
+Ne fillim e mapojme secilen leter duke u bazuar ne suitin e saj, duke e marre vleren modulo 4.  
+Pastaj nga suits e krijon nje set. Nese madhesia e setit eshte 1, atehere kemi letra te te njejtit suit dhe kemi flush.
+ */
 
 function countRanks(cards){ //funksion i cili i mbledh te gjitha dukurite/shfaqjet (occurences) te cdo rangu
     const ranks = {};
@@ -36,7 +54,11 @@ function countRanks(cards){ //funksion i cili i mbledh te gjitha dukurite/shfaqj
         ranks[rank] = ranks[rank] ? ranks[rank] + 1 : 1;
     });
     return ranks;
-}
+}/* Funksion `countRanks` eshte funksion i cili i mbledh te gjitha dukurite/shfaqjet (occurences) te cdo rangu.
+Ne fillim inicializojme nje objekt te thate te quajtur rank, dhe iteron mbi cdo leter, e kalkulojme rankun e saj
+duke e ndare me 4 dhe duke e marre floor value, dhe pastaj e bejme update the count ne ranks object.
+nese ranku ekziston ne objektin ranks, e inkrementojme countin, perndryshe e inicializojme countin me 1.
+ */
 
 function determineHandRanking(cards){
     const rankCounts = countRanks(cards);
@@ -68,6 +90,13 @@ function determineHandRanking(cards){
         return 0; // High card
     }
 }
+/*Funksioni determineHandRanking vendos rankun e nje dore te letrave. Ne fillim i ngjen te gjitha rastet e secilit rank duke
+perdorur funksioni countRanks. Pastaj, shikon kufi te ndryshem per te determinuar rankimit. 
+Nese dora eshte eshte straight edhe flush, atehere shikon nese eshte Royal Flush, duke shikuar nese permban ranqet 8,9,10,11,12
+te cilat reprezentojne 10, J, Q, K, A. Nese eshte kthen 9, nese nuk eshte atehere kthen 8 per straight flush.
+Pastaj, vazhdon te kontrolloj te gjitha kufizimet tjera, dhe nese nuk eshte asnjera nga to, e kthen 0 e cila reprezenton
+High Card apo Nothing.*/
+
 function calculateProbabilities(){
     const allCards = Array.from({length: 52}, (_, i) => i);
     const allCombinations = combinations(allCards, 5);
@@ -99,6 +128,17 @@ function calculateProbabilities(){
     rankedHands.sort((a, b) => b.probability - a.probability);
     return rankedHands;
 }
+/* Funksioni calculateProbabilities eshte funksion i cili llogarit probabilitetin e cdo lloji te poker hands.
+Ne fillim, e krijojme nje varg te quajtur allCards i cili i ka te gjitha kartat nga 0 deri 51.
+Pastaj, i gjeneron te gjitha kombinimet e ketyre 5 letrave, duke perdorur funksionin combinations.
+E inicializon nje varg te quajtur handCounts me 10 elemente, secili duke reprezentuar numrin e rankut te nje dore specifike.
+Funksioni iteron mbi te gjitha kombinimet, duke e determinuar rankun e seciles dore, dhe duke e inkrementuar countin e rankut te doreve.
+Pastaj, llogarit totalin e duarve duke marr gjatesine e vargut allCombinations. 
+Me pas, e kalkulon probabilitetin e secilit rank duke e ndare countin e secilit rank me totalin e duarve, si ne formule.
+Si perfundim, e krijon nje varg te quajtur rankedHands qe permban nje objekt me emrin e dores dhe probabilitetin e saj..
+E merr kete varg, dhe e sorton ne descending order, duke e kthyer si rezultat ne baze te probabilitetit.
+
+ */
 
 function main() {
     const rankedHands = calculateProbabilities();
@@ -109,3 +149,8 @@ function main() {
 }
 
 main();
+/* Funksioni main eshte "entry point" i programit. E thirr funksionit calculateProbabilities per
+te marr duart e rankuara dhe probabilitetitn e tyre, dhe e printon rezultatin duke perdorur console.log
+Cdo rresht permban rankun (me e larta tek me e uleta), probabilitetin (e formatuar si perqindje me 6 vende decimale)
+dhe numrin e dores.
+ */
